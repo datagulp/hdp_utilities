50e3c9904c92c725afaddad03d11c20d4f2e99ed
root <root@grj-2.field.hortonworks.com>
2016-12-21 22:53:51 +0000

    Updated to run in parallel

--- teragensort.sh
+++ teragensort.sh
@@ -3,15 +3,16 @@ echo "Starting Teragen execution ... "
 mapmem=1024
 for mtask in 16 32 64 128
 do
-    hadoop fs -rm -r -skipTrash /tmp/teragen-output
-    hadoop jar /usr/hdp/2.4.2.0-258/hadoop-mapreduce/hadoop-mapreduce-examples.jar teragen -Dmapred.map.tasks=$mtask -Dmapreduce.map.memory.mb=$mapmem 1000000000 /tmp/teragen-output
+    hadoop fs -rm -r -skipTrash /tmp/teragen-output${mtask}
+    hadoop jar /usr/hdp/current/hadoop-mapreduce-client/hadoop-mapreduce-examples.jar teragen -Dmapred.map.tasks=$mtask -Dmapreduce.map.memory.mb=$mapmem 1000000000 /tmp/teragen-output${mtask} &
 done
+
+read "  Press Ctrl+c to exit ... Enter will run terasort .. " 
 
 echo "Starting Terasort execution ... "
 rmem=3096
 for rtask in 16 32 64
 do
-    hadoop fs -rm -r -skipTrash /tmp/terasort-output
-    hadoop jar /usr/hdp/2.4.2.0-258/hadoop-mapreduce/hadoop-mapreduce-examples.jar terasort  -Dmapred.reduce.tasks=$rtask -Dmapreduce.reduce.memory.mb=$rmem  /tmp/teragen-output /tmp/terasort-output
+    hadoop fs -rm -r -skipTrash /tmp/terasort-output${rtask}
+    hadoop jar /usr/hdp/current/hadoop-mapreduce-client/hadoop-mapreduce-examples.jar terasort  -Dmapred.reduce.tasks=$rtask -Dmapreduce.reduce.memory.mb=$rmem  /tmp/teragen-output /tmp/terasort-output${rtask} 
 done
-
