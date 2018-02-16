dfe4158c1785aaa3d6611f35b1bef87f874f7055
Ganesh Rajagopal <ganesh_rajagopal@hotmail.com>
2018-02-16 14:25:31 -0500

    Update nodeprepscript.sh

--- nodeprepscript.sh
+++ nodeprepscript.sh
@@ -28,7 +28,7 @@ yum install -y snappy-1.0.5* >> $LOGFILE
 
 #echo "Install JDK" >> $LOGFILE
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jdk-8u102-linux-x64.rpm" -O /tmp/jdk-8u102-linux-x64.rpm
-
+## JDK 1.7.51 link --> http://download.oracle.com/otn/java/jdk/7u55-b13/jdk-7u55-linux-i586.rpm
 cd /tmp
 
 rpm -ivh jdk-8u102-linux-x64.rpm
